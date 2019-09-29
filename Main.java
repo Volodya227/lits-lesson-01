0af2876b0ed280ae20302882e4e7868c90458ef0
Volodya227 <55955806+Volodya227@users.noreply.github.com>
2019-09-29 20:42:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+        var a=2; var b=3; var c=4; var d=2; var e=2;
+        System.out.println((a+b)/(d-b)*2);
+        System.out.println(a+2*2-12/(a*(b-c)));
+        System.out.println(b+11*c-a);
+        System.out.println(a*(c+b+d)-e/1+a);
+        System.out.println(a+b*(a-b)/12);
+
+
+
+    }
+
+}
